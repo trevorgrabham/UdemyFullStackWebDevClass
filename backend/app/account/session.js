b2837b00e85a39846868efa3c07513cbc0b7bb89
trevorgrabham <tgrabham@sfu.ca>
2021-12-23 16:10:37 -0800

    Added verification to our Session class
    Added static functions verify(sessionString) and parse(sessionString) to allow us to verify that the sessionString is valid and to allow us to retrieve the username and id from the sessionString respectively.

--- backend/app/account/session.js
+++ backend/app/account/session.js
@@ -1,6 +1,8 @@
 const uuid = require('uuid/v4');
 const { hash } = require('./helper.js');
 
+const SEPERATOR = '|';
+
 class Session {
     constructor({ username }) { 
         this.username = username;
@@ -13,14 +15,32 @@ class Session {
         return Session.sessionString({ username, id });
     }
 
+    static parse(sessionString) {
+        const sessionData = sessionString.split(SEPERATOR);
+
+        return {
+            username: sessionData[0],
+            id: sessionData[1],
+            sessionHash: sessionData[2]
+        };
+    }
+
+    static verify(sessionString) {
+        const { username, id, sessionHash } = Session.parse(sessionString);
+
+        const accountData = Session.accountData({ username, id });
+
+        return hash(accountData) === sessionHash;
+    }
+
     static accountData({ username, id}) {
-        return `${username}|${id}`;
+        return `${username}${SEPERATOR}${id}`;
     }
 
     static sessionString({ username, id}) {
         const accountData = Session.accountData({ username, id });
 
-        return `${accountData}|${hash(accountData)}`
+        return `${accountData}${SEPERATOR}${hash(accountData)}`
     }
 }
 
