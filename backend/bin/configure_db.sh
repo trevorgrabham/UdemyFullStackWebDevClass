b3ac251329b1307a2a4554959b937c03013bc775
trevorgrabham <tgrabham@sfu.ca>
2021-12-25 15:40:14 -0800

    Added a new table into our DB for linking dragons to accounts
    New accountDragon table keeps track of dragonId and accountId so that each dragon will have an owner.

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -12,6 +12,7 @@ psql -U node_user dragonstackdb < ./bin/sql/dragon.sql
 psql -U node_user dragonstackdb < ./bin/sql/trait.sql
 psql -U node_user dragonstackdb < ./bin/sql/dragonTrait.sql
 psql -U node_user dragonstackdb < ./bin/sql/account.sql
+psql -U node_user dragonstackdb < ./bin/sql/accountDragon.sql
 
 node ./bin/insertTraits.js 
 
