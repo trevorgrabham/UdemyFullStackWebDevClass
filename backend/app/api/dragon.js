50efa01f18d3cc8993ee1fda21ed72b0bee2bcc6
trevorgrabham <tgrabham@sfu.ca>
2021-12-26 14:33:18 -0800

    Finished implementation of automatically storing entry in accountDragon on our DB when a new dragon is created
    Updated the /api/dragon.js file to store the entry as soon as the dragon is created.
    Updated the /actions/dragon.js file in the frontend so that we are sending cookie data to our backend API when we are accessing the /dragon/new endpoint

--- backend/app/api/dragon.js
+++ backend/app/api/dragon.js
@@ -4,28 +4,26 @@
 
 const {Router} = require('express');
 const DragonTable = require('../dragon/table.js');
-const DragonTraitTable = require('../dragonTrait/table.js');
-const {getDragonWithTraits} = require('../dragon/helper.js');
+const AccountDragonTable = require('../accountDragon/table.js');
+const { authenticatedAccount } = require('./helper.js');
 
 const router = Router();
 
 router.get("/new", (req, res, next) => {
-    const dragon = req.app.locals.generationEngine.generation.newDragon();
+    let accountId, dragon;
 
-    DragonTable.storeDragon(dragon)
-    .then(({ dragonId }) => {
-        dragon.id = dragonId;
-
-        console.log('new dragon being created');
-        console.log('new dragon', dragon);
-
-        res.json({ dragon: dragon });
-    })
-    .catch((error) => {
-        next(error);
-    });
-
-
+    authenticatedAccount({ sessionString: req.cookies.sessionString })
+        .then(({ account }) => {
+            accountId = account.id;
+            dragon = req.app.locals.generationEngine.generation.newDragon();
+            return DragonTable.storeDragon(dragon);
+        })
+        .then(({ dragonId }) => {
+            dragon.id = dragonId;
+            return AccountDragonTable.storeAccountDragon({ accountId, dragonId });
+        })
+        .then(() => res.json({ dragon }))
+        .catch((error) => next(error));
 });
 
 module.exports = router;
